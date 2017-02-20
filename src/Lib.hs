30d3a2a11026f69fb6c7b74b4869cf8d16cc86fc
Janos Potecki <janos.potecki.15@ucl.ac.uk>
2017-02-20 22:05:41 +0000

    Calculating all paths seems to work

--- src/Lib.hs
+++ src/Lib.hs
@@ -4,12 +4,13 @@ module Lib
 
 import Math1
 import Geom2D
+import Data.List
 
 robots :: [Point Double]
 robots =  fmap tuple2Point rawRobot
 
 rawRobot :: [(Double, Double)]
-rawRobot = (-1,-1):(4,4):[]
+rawRobot = [(0,1),(2,0),(3,5),(6,2),(9,0)]
 
 toPolygon :: [(Double, Double)] -> Polygon Double
 toPolygon xs = Polygon $ tuple2Point <$> xs
@@ -20,19 +21,24 @@ polygons :: [Polygon Double]
 polygons = toPolygon <$> rawPolygons'
 
 rawPolygons' :: [ [(Double, Double)] ] 
-rawPolygons' =  [ [(1,2),(1,4),(3,4),(3,2) ]
-                , [(8,1),(4,1),(4,4),(5,2) ]
+rawPolygons' =  [ [(1,2),(1,4),(3,4),(3,2)]
+                , [(8,1),(4,1),(4,4),(5,2)]
                 ]
 
 someFunc :: IO ()
 someFunc = do
-    let start  = robots !! 0
-        target = robots !! 1
-    putStrLn "Printing polygons"
-    -- print $ (toPolygon) <$> rawPolygons'
-    print  $ getAllObstacleLines polygons
-    putStrLn "done printing"
-    path   <- getPath EmptyPath start target (getAllObstacleLines polygons)
-    putStrLn "Result:"
-    print path
+    let robotPermutations = (,) <$> init robots <*> tail robots
+        polygons'         = getAllObstacleLines polygons
+        getPath' = \(r1,r2) -> getPath EmptyPath r1 r2 polygons'
+    print $ nub robotPermutations
+    mapM getPath' robotPermutations >>= mapM_ (print . normalizePath)
+    -- let start  = robots !! 0
+    --     target = robots !! 1
+    -- putStrLn "Printing polygons"
+    -- -- print $ (toPolygon) <$> rawPolygons'
+    -- print  $ getAllObstacleLines polygons
+    -- putStrLn "done printing"
+    -- path   <- getPath EmptyPath start target (getAllObstacleLines polygons)
+    -- putStrLn "Result:"
+    -- print $ normalizePath path
 
--- src/Math1.hs
+++ src/Math1.hs
@@ -27,6 +27,12 @@ join p1 EmptyPath = p1
 join EmptyPath p2 = p2
 join p1 p2 = Path (path p1 ++ path p2) (dist p1 + dist p2)
 
+normalizePath :: Eq a => Path a -> Path a
+-- ^ If two consequtive points are the same, throw them out
+normalizePath Path{..} = Path path' dist
+  where
+    path' = filter (\(Line x y) -> if x == y then False else True) path
+
 getP1 :: Line a -> Point a
 getP1 (Line p _) = p
 
@@ -38,8 +44,10 @@ tuple2Point (x, y) = Point x y
 
 transformPolygon :: Polygon a -> [Line a]
 -- ^ transfors a poligon to a list of lines
-transformPolygon (Polygon xs) = do
-    zipWith (Line) xs xs'
+transformPolygon (Polygon xs) = permutationOfPairsWith (Line) xs
+
+permutationOfPairsWith :: (a -> a -> b) -> [a] -> [b]
+permutationOfPairsWith f xs = zipWith f xs xs'
   where
     xs' = (last xs) : init xs
 
@@ -58,13 +66,20 @@ intersect :: (Ord a, Floating a) => Line a -> Line a
 intersect l1@(Line start target) l2 = do
     let intersecPoint = lineIntersect l2 l1 0
     case intersecPoint of 
-        Nothing -> Nothing
-        Just x  -> if (isInScope x l2)&&(isInScope x l1)&&not(isInScope target l2)
-                   then Just (vectorDistance x start, l2)
-                   else Nothing
+      Nothing -> Nothing
+      Just x  -> if isInScope l1 l2 target x
+                 then Just (vectorDistance x start, l2)
+                 else Nothing
 
-isInScope :: (Ord a, Floating a) => Point a -> Line a -> Bool
-isInScope p (Line p1 p2) = 
+isInScope :: (Ord a, Floating a) => Line a -> Line a 
+                                 -> Point a -- Target point 
+                                 -> Point a -- Intersection Point
+                                 -> Bool
+isInScope l1 l2 t p = 
+    isInScope' p l2 && isInScope' p l1 && not (isInScope' t l2)
+
+isInScope' :: (Ord a, Floating a) => Point a -> Line a -> Bool
+isInScope' p (Line p1 p2) = 
         linedist == (vectorDistance p p1) + (vectorDistance p p2) 
   where linedist = vectorDistance p1 p2
 
@@ -80,26 +95,26 @@ findMinDist ls = snd $ minimumBy minimumP ls
     -- distance line  = (lineDistance line p, line)
 --------------------------------------------------------------------------------
 getPath :: (Show a, Ord a, Floating a) => Path a  -- Accumulator
-                               -> Start a-> Target a -> [Line a] 
+                               -> Start a -> Target a -> [Line a] 
                                -> IO (Path a)  -- Result
 -- ^ takes two points and returns a paths
 getPath acc start target obst = do
     let line          = Line start target
         intersections = findAllIntersections line obst
-    putStrLn "printing intersections"
-    mapM print intersections
-    putStrLn "------------------------------------"
-    print acc 
-    putStrLn "------------------------------------"
-
+    -- putStrLn "printing intersections"
+    -- mapM print intersections
+    -- putStrLn "------------------------------------"
+    -- print acc 
+    -- putStrLn "------------------------------------"
     case intersections of
         [] -> return $ join acc $ Path [line] (vectorDistance start target)
         xs -> do
-            let acc' =  join acc $ Path [line] (vectorDistance start target)
             let nextLine = findMinDist xs
-            putStr "going to point:" 
-            print $ getP2 nextLine
-            -- right <-getPath acc (getP1 nextLine) target (obst \\ [nextLine])
-            left  <- getPath acc' (getP2 nextLine) target (obst \\ [nextLine])
-            -- return $ join acc $ min left right
-            return $ join acc left
+                pR       = getP1 nextLine
+                pL       = getP2 nextLine
+                accL = Path [Line start pL] (vectorDistance start pL)
+                accR = Path [Line start pR] (vectorDistance start pR)
+            right <- getPath accR pR target (obst \\ [nextLine])
+            left  <- getPath accL pL target (obst \\ [nextLine])
+            return $ join acc $ min left right
+            -- return $ join acc left
